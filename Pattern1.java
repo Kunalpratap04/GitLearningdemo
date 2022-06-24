8c270595c80b51d61033f5f9f183978e5286721d
Kunal Pratap Singh Pawar <kunalpratapsingh.28@gmail.com>
2022-06-24 18:18:07 +0530

    changed

---
+++ Pattern1.java
@@ -0,0 +1,25 @@
+package Patterns;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print(5);
+		
+
+	}
+	static void print(int n)
+	{
+		int a=0;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n+1-i;j++)
+			{
+				System.out.print(j);
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
